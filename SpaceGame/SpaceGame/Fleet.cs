27d26182fb98a066beab7973d39ceb656152a4c0
Shadow4896 <thomac59@my.erau.edu>
2020-08-04 11:35:38 -0500

    Created Story and Fleet class

---
+++ SpaceGame/SpaceGame/Fleet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SpaceGame
+{
+    public enum SpaceShips { StarFighter, SalvageHauler, Frigit }
+   
+    class Fleet
+    {
+        
+    }
+}
---
+++ SpaceGame/SpaceGame/Story.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceGame
+{
+    class Story
+    {
+        string userName;
+        public void Run()
+        {
+            Console.WriteLine("Welcome to Get Rich or Die Trying \nPlease enter your characters name");
+            userName = Console.ReadLine();
+            Console.WriteLine($" Hello {userName}, are you ready to get money or die trying?");
+            Console.WriteLine("You have 42 years to obtain your fortune by traveling from planet to planet");
+        }
+    }
+}
