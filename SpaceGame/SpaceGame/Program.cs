87a2cd5ed5a7ab0e3dbf71863e3e45f3e83e51a2
Shadow4896 <thomac59@my.erau.edu>
2020-08-05 09:58:22 -0500

    Updated story and deleted duplicate class

--- SpaceGame/SpaceGame/Program.cs
+++ SpaceGame/SpaceGame/Program.cs
@@ -2,27 +2,13 @@
 
 namespace SpaceGame
 {
-<<<<<<< HEAD
 
     class Program
     {
         static void Main(string[] args)
         {
             new Story().Run();
-               
-        }
-    }
-=======
-    class program
-    {
-        static void Main(string[] args)
-        {
 
         }
     }
-
-    
-    
-    
->>>>>>> 0414c49a36e775fc9416344a309dc7a67a594837
 }
--- SpaceGame/SpaceGame/Story.cs
+++ SpaceGame/SpaceGame/Story.cs
@@ -9,10 +9,12 @@ namespace SpaceGame
         string userName;
         public void Run()
         {
-            Console.WriteLine("Welcome to Get Rich or Die Trying \nPlease enter your characters name");
+            Console.WriteLine("Welcome to Get Rich or Die Trying\n---------------------------------\n\nPlease enter your characters name : ");
             userName = Console.ReadLine();
-            Console.WriteLine($" Hello {userName}, are you ready to get money or die trying?");
-            Console.WriteLine("You have 42 years to obtain your fortune by traveling from planet to planet");
+            Console.WriteLine($"\nHello {userName}, are you ready to get money or die trying? \n\nThe object of this game is to accumulate wealth as fast and efficiently as possible.\n");
+            Console.WriteLine("You have 42 years to obtain your fortune by traveling from planet to planet selling and acquiring goods");
+            Console.ReadLine();
+            Console.WriteLine("Please select a ship to start off your journey \n1. StarFighter (Speed : Warp 3, Capacity: 1000lbs)\n2. SalvageHauler (Speed : Warp 2, Capacity: 2500lbs)\n3. Frigate (Speed : Warp 1, Capacity: 5000lbs)\n");
         }
     }
 }
